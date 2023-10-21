11e758e96afee2cd4855b5a443a7d3f817f6e612
Sadman9061 <sadmansakib.dev@gmail.com>
2023-10-21 19:58:23 +0600

    Prepare the server-side code for deployment, including setting up environment variables and configuring the server for production.

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const cors = require("cors");
 const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
-// const jwt = require(
+
 require("dotenv").config(); // import env file if use environment variables after install || npm install dotenv --save|| ane Create a .env file in the root of your project:
 const port = process.env.PORT || 5000;
 const app = express();
